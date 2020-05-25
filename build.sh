3d732ad14851fdf8fb332ebe051b3ac50b489db7
Sathish <sathishkumar.palani26@gmail.com>
2020-05-25 11:59:20 +0530

    New Project

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-
-sudo docker build ./mysql/ -t db
-sudo docker run -it -d -p 3306:3306 --name mysql db
-
-sudo docker build ./website/ -t web
-sudo docker run -it -d -p 3001:80 --link mysql:db web
+sudo docker build ./website/ -t app
+sudo docker run -it -d -p 3001:80 --name web app
